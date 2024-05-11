e9836195bce141f187f3ffc3918773e8045fbfce
menthy-wu <wumengxuan0728@gmail.com>
2024-05-11 15:34:07 -0700

    add min

--- src/components/RandomImage.jsx
+++ src/components/RandomImage.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const RandomImage = ({ src, img_style, color }) => {
   return (
-    <div className={`absolute max-w-[150px]`} style={img_style}>
+    <div className={`absolute max-w-[200px] min-w-[100px]`} style={img_style}>
       <Image src={src} alt="img" className={`border-4 rounded-2xl ${color}`} />
     </div>
   );
